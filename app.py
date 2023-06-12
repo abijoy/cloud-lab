4e1d475ffb7a2f45a356b9c49214e1832667f080
Md Alamin <mdalaminbijoy3@gmail.com>
2023-06-13 01:28:50 +0600

    even numbers

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+@app.route('/even_numbers/<int:n>', methods=['GET'])
+def generate_even_numbers(n):
+    if n < 0:
+        return jsonify({'error': 'Invalid input. Please provide a non-negative integer.'}), 400
+    
+    even_numbers = [i for i in range(2, (n * 2) + 1, 2)]
+    
+    return jsonify({'even_numbers': even_numbers})
+
+
+# def hello_world():
+#     return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run()
